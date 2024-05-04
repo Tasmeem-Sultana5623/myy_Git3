322fc8dfb5c65f789026b94ebae143a09f9bad75
Tasmeem-Sultana5623 <tasmeemsultana209@gmail.com>
2024-05-04 23:50:57 +0600

    added all files

---
+++ script.js
@@ -0,0 +1,37 @@
+//how to connect js and html?
+
+// Ans: "<body>
+   // <script src="./script.js">
+
+   // </script>//
+
+
+
+   //How to write variable in javascript? write at least 3 variable?
+
+
+   //Ans: 
+   //var a =80
+   //let Fullname= "Tasmeem sultana"//let secondnumer=30; 
+   //const s = 9.458;
+
+
+   //  3. what is the difference between let, var, const variable ?
+
+   //Ans: let doesnt take the same variables repeatedly.
+   //such as : 
+
+
+   ////let  secondnumber=30 //Number;
+   //let  secondnumber2=60 //Number;
+   //let  secondnumber3=20 //Number;
+
+   //console.log(secondnumber);
+      //console.log(secondnumber2);
+         //console.log(secondnumber3);
+
+         
+
+   
+
+   
